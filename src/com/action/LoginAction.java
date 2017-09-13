40dea1ac7dbe8fd84c421a495f2c12a2c19c80ac
charongfu <rongfu97@outlook.com>
2017-09-13 19:39:18 +0800

    struts框架雏形

---
+++ src/com/action/LoginAction.java
@@ -0,0 +1,15 @@
+package com.action;
+
+import org.springframework.web.struts.ActionSupport;
+
+
+public class LoginAction extends ActionSupport {
+	/**
+	 * 方法描述：注册一个Action（登录的Action），当前台请求的时候，通过过滤器调用该方法
+	 * 
+	 **/
+	public String execute() throws Exception {
+		return "success";
+	}
+
+}
